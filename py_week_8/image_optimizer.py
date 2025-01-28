e304215752ee04adbfd2a731cc3da27011dd5321
Ollie <Ollie_0@mail.ru>
2025-01-28 11:38:47 +0700

    Added constants and defined a function

--- py_week_8/image_optimizer.py
+++ py_week_8/image_optimizer.py
@@ -5,11 +5,30 @@ from pillow_heif import register_heif_opener, from_pillow as heif_from_pillow
 from pillow_avif import register_avif_opener, from_pillow as avif_from_pillow
 from tqdm import tqdm
 
-# Регистрируем форматы
+# Registered image openers
 register_heif_opener()
 register_avif_opener()
 
+# Added constants
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'JPG', 'JPEG'}
 DEFAULT_QUALITY = 40
 DEFAULT_FORMAT = 'AVIF'
 AVAILABLE_FORMATS = {'WEBP', 'HEIF', 'AVIF'}
+
+def get_images_paths(source_path: str) -> List[str]:
+    """
+    Returnns a list of image paths in the specified directory.
+    """
+    if not os.path.exists(source_path):
+        raise FileNotFoundError(f"Путь '{source_path}' не существует.")
+
+    image_paths = []
+    for root, _, files in os.walk(source_path):
+        for file in files:
+            if file.split('.')[-1] in ALLOWED_EXTENSIONS:
+                image_paths.append(os.path.join(root, file))
+
+    if not image_paths:
+        raise ValueError("Не найдено подходящих изображений в указанной директории.")
+
+    return image_paths
