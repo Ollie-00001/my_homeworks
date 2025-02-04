821a34280393c8d6ec4b44d3123636dca94df529
Ollie <Ollie_0@mail.ru>
2025-02-04 21:02:39 +0700

    Defined append function for JSON files

--- py_week_9/files_utils.py
+++ py_week_9/files_utils.py
@@ -15,3 +15,11 @@ def write_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
     except (TypeError, json.JSONDecodeError) as e:
         print(f"Ошибка записи JSON-файла {file_path}: {e}")
 
+def append_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
+    existing_data = read_json(file_path, encoding) or []
+    if not isinstance(existing_data, list):
+        print(f"Ошибка: JSON файл {file_path} должен содержать список.")
+        return
+    existing_data.extend(data)
+    write_json(*existing_data, file_path=file_path, encoding=encoding)
+
