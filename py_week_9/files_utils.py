3f648480f2337ecc3662537ff6cc800160d7bdc7
Ollie <Ollie_0@mail.ru>
2025-02-04 17:56:05 +0700

    Defined write function for JSON files

--- py_week_9/files_utils.py
+++ py_week_9/files_utils.py
@@ -4,3 +4,6 @@ def read_json(file_path: str, encoding: str = 'utf-8'):
     with open(file_path, encoding=encoding) as f:
         return json.load(f)
 
+def write_json(file_path: str, data, encoding: str = 'utf-8'):
+    with open(file_path, 'w', encoding=encoding) as f:
+        json.dump(data, f, ensure_ascii=False)
