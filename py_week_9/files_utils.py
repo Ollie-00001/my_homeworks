c5c3af9d0fb831f517625373029fd4ed3fae2bc8
Ollie <Ollie_0@mail.ru>
2025-02-04 18:31:12 +0700

    Added None type annotations

--- py_week_9/files_utils.py
+++ py_week_9/files_utils.py
@@ -5,11 +5,11 @@ def read_json(file_path: str, encoding: str = 'utf-8'):
     with open(file_path, encoding=encoding) as f:
         return json.load(f)
 
-def write_json(file_path: str, data, encoding: str = 'utf-8'):
+def write_json(file_path: str, data, encoding: str = 'utf-8') -> None:
     with open(file_path, 'w', encoding=encoding) as f:
         json.dump(data, f, ensure_ascii=False)
 
-def append_json(*data, file_path: str, encoding: str = 'utf-8'):
+def append_json(*data, file_path: str, encoding: str = 'utf-8') -> None:
     with open(file_path, 'a', encoding=encoding) as f:
         json.dump(data, f, ensure_ascii=False)
 
@@ -17,12 +17,12 @@ def read_csv(file_path: str, delimiter: str = ';', encoding: str = 'utf-8'):
     with open(file_path, encoding=encoding) as f:
         return list(csv.reader(f, delimiter=delimiter))
     
-def write_csv(*data, file_path: str, delimiter: str = ';', encoding: str = 'utf-8'):
+def write_csv(*data, file_path: str, delimiter: str = ';', encoding: str = 'utf-8') -> None:
     with open(file_path, 'w', encoding=encoding) as f:
         writer = csv.writer(f, delimiter=delimiter)
         writer.writerows(data)
         
-def append_csv(*data, file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig'):
+def append_csv(*data, file_path: str, delimiter: str = ';', encoding: str = 'utf-8-sig') -> None:
         with open(file_path, 'a', encoding=encoding) as f:
             csv.dump(data, f, ensure_ascii=False)
 
