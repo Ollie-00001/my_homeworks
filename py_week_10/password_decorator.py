db105408766a05ffa1e07a0a121336b5d2d6130a
Ollie <Ollie_0@mail.ru>
2025-02-05 17:50:46 +0700

    Modified the decorator

--- py_week_10/password_decorator.py
+++ py_week_10/password_decorator.py
@@ -1,10 +1,16 @@
+from typing import Callable
 
-def password_checker(func):
-    def wrapper(*args, **kwargs):
-        password = input("Enter the password: ")
-        if password == '12345':
-            return func(*args, **kwargs)
-        else:
-                print("Incorrect password. Access denied.")
-        return wrapper
-
+def password_checker(func: Callable) -> Callable:
+    def wrapper(password: str) -> str:
+        if len(password) < 8:
+            return "Пароль должен содержать минимум 8 символов."
+        if not any(char.isdigit() for char in password):
+            return "Пароль должен содержать хотя бы одну цифру."
+        if not any(char.isupper() for char in password):
+            return "Пароль должен содержать хотя бы одну заглавную букву."
+        if not any(char.islower() for char in password):
+            return "Пароль должен содержать хотя бы одну строчную букву."
+        if not any(char in '!@#$%^&*()-_+=<>?/.,:;{}[]|' for char in password):
+            return "Пароль должен содержать хотя бы один специальный символ."
+        return func(password)
+    return wrapper
