75776d5ff72e26362e3b71339273d473cc261f49
Ollie <Ollie_0@mail.ru>
2025-03-04 17:02:57 +0700

    Dict to List[Dict]

--- py_week_12/marvel_processed.py
+++ py_week_12/marvel_processed.py
@@ -2,7 +2,8 @@ from marvel import full_dict
 from pprint import pprint
 from typing import List, Dict, Set, Optional
 
-# 2. Получаем список ID фильмов, вводимых пользователем
-user_input = input("Введите ID фильмов через пробел: ")
-id_list: List[Optional[int]] = list(map(lambda x: int(x) if x.isdigit() else None, user_input.split()))
 
+user_input = input("Введите ID фильмов через пробел: ")
+id_list: List[Optional[int]] = list(map(lambda x: int(x) if x.isdigit() else None, user_input.split())) # Получаем список ID фильмов, вводимых пользователем
+
+movies_list: List[Dict] = [{"id": key, **value} for key, value in full_dict.items()] # Перепакуем full_dict в список словарей
