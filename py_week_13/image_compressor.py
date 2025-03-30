a1536d13fa123adc71e9cfa570c96c97aed5daa8
Ollie <Ollie_0@mail.ru>
2025-03-30 10:28:24 +0700

    Added main function

--- py_week_13/image_compressor.py
+++ py_week_13/image_compressor.py
@@ -40,3 +40,26 @@ class ImageCompressor:
                     input_path = os.path.join(root, file)
                     output_path = os.path.splitext(input_path)[0] + '.heic'
                     self.compress_image(input_path, output_path)
+
+def main(input_path: str) -> None: # Основная функция программы
+    """
+    Args:
+        input_path (str): Путь к файлу или директории.
+    """
+    input_path = input_path.strip('"')
+    compressor = ImageCompressor(quality=50)
+    
+    if os.path.exists(input_path):
+        if os.path.isfile(input_path):
+            print(f"Обрабатываем файл: {input_path}")
+            output_path = os.path.splitext(input_path)[0] + '.heic'
+            compressor.compress_image(input_path, output_path)
+        elif os.path.isdir(input_path):
+            print(f"Обрабатываем директорию: {input_path}")
+            compressor.process_directory(input_path)
+    else:
+        print("Указанный путь не существует")
+
+if __name__ == "__main__":
+    user_input = input("Введите путь к файлу или директории: ")
+    main(user_input)
