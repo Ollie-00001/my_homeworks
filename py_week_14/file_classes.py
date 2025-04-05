7b3c1ebf32b925dd9b4062d8169e9d864da87187
Ollie <Ollie_0@mail.ru>
2025-04-05 08:59:15 +0700

    Initial commit

---
+++ py_week_14/file_classes.py
@@ -0,0 +1,29 @@
+import json
+from abc import ABC, abstractmethod
+
+class AbstractFile(ABC):
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+    @abstractmethod
+    def append(self, data):
+        pass
+
+class JsonFile(AbstractFile):
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def read(self):
+        try:
+            with open(self.file_path, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            return None
+        except json.JSONDecodeError:
+            return None
+    
