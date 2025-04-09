311efd8be4328fa27d7e19a68030f8da6ad7673d
Ollie <Ollie_0@mail.ru>
2025-04-09 07:16:37 +0700

    TXT file handler

--- py_week_14/file_classes.py
+++ py_week_14/file_classes.py
@@ -42,3 +42,20 @@ class JsonFile(AbstractFile):
         else:
             content = [data]
         self.write(content)
+
+class TxtFile(AbstractFile):
+
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read(self) -> str:
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            return f.read()
+
+    def write(self, data: str) -> None:
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            f.write(data)
+
+    def append(self, data: str) -> None:
+        with open(self.file_path, 'a', encoding='utf-8') as f:
+            f.write(data)
