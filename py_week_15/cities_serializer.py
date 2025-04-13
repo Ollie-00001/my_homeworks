0136994ca0bca11cad61b14e3e30f6fd34fc4a8c
Ollie <Ollie_0@mail.ru>
2025-04-13 20:09:00 +0700

    CitiesSerializer logic

---
+++ py_week_15/cities_serializer.py
@@ -0,0 +1,22 @@
+from typing import List
+from city import City
+
+class CitiesSerializer:
+    def __init__(self, city_data: List[dict]) -> None:
+        self.cities = List[City]
+        for entry in city_data:
+            try:
+                city = City(
+                    name=entry['name'],
+                    population=entry['population'],
+                    subject=entry['subject'],
+                    disctrict=entry['disctrictt'],
+                    latitude=entry['latitude'],
+                    longitude=entry['longitude'],
+                    is_used=entry['is_used']
+                )
+                self.cities.append(city)
+            except ValueError as e:
+                print(f'Error creating city: {e}')
+    def get_all_cities(self) -> List[City]:
+        return self.cities
