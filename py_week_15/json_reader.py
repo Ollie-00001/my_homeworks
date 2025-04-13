e2cb59058d5d6e53f60ed315dc2612b59c66f9ab
Ollie <Ollie_0@mail.ru>
2025-04-13 19:56:20 +0700

    Created a class with methods for reading JSON

---
+++ py_week_15/json_reader.py
@@ -0,0 +1,21 @@
+import json
+from typing import Any, List
+
+class JSONReader:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+
+    def read_file(self) -> List[dict[str, Any]]:
+        try:
+            with open(self.file_path, 'r', encoing='utf-8') as f:
+                data = json.load(f)
+                return data
+        except FileNotFoundError:
+            print(f'File {self.file_path} not found')
+            return []
+        except json.JSONDecodeError:
+            print(f'Error decoding JSON in file {self.file_path}')
+            return []
+        except Exception as e:
+            print(f'Error reading file {self.file_path}: {e}')
+            return []
