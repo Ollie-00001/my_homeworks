9a943cf2bef479040a23c16c1b556ee9d491339f
Ollie <Ollie_0@mail.ru>
2025-04-13 20:38:55 +0700

    cities

--- py_week_15/main.py
+++ py_week_15/main.py
@@ -2,7 +2,7 @@ from json_reader import JSONReader
 from cities_serializer import CitiesSerializer
 
 def main():
-    reader = JSONReader('cities.json')
+    reader = JSONReader('C:\Main\my_projects\python_413\my_homeworks\py_week_15\cities.json')
     city_data = reader.read_file()
 
     serializer = CitiesSerializer(city_data)
