004e3c67d2244b3cf31ca6602b3f0897a810b12d
Ollie <Ollie_0@mail.ru>
2025-04-17 10:37:13 +0700

    fixed a bug

--- py_week_15/main.py
+++ py_week_15/main.py
@@ -1,15 +1,15 @@
 from json_reader import JSONReader
 from cities_serializer import CitiesSerializer
+from city_game import CityGame
+from manager import GameManager
 
 def main():
-    reader = JSONReader('C:\Main\my_projects\python_413\my_homeworks\py_week_15\cities.json')
-    city_data = reader.read_file()
+    json_file = JSONReader("C:\Main\my_projects\python_413\my_homeworks\py_week_15\cities.json")
+    cities_data = json_file.read_file()
+    cities_serializer = CitiesSerializer(cities_data)
+    city_game = CityGame(cities_serializer)
+    game_manager = GameManager(json_file, cities_serializer, city_game)
+    game_manager()
 
-    serializer = CitiesSerializer(city_data)
-    cities = serializer.get_all_cities()
-
-    for city in cities:
-        print(city)
-    
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
