78ace16a9fbf861af82ce3bcbf4932597f297f93
Ollie <Ollie_0@mail.ru>
2025-04-17 09:59:15 +0700

    method run_game

--- py_week_15/manager.py
+++ py_week_15/manager.py
@@ -10,3 +10,24 @@ class GameManager:
 
     def __call__(self):
         self.run_game()
+
+    def run_game(self):
+        print("Добро пожаловать в игру 'Города'!")
+        while True:
+            player_input = input("Введите название города: ").strip().lower()
+            if not self.city_game.human_turn(player_input):
+                print("Неверный ввод. Попробуйте снова.")
+                break
+    
+            if self.city_game.check_game_over():
+                print("Поздравляем! Вы победили, у компьютера закончились варианты.")
+                break
+
+            if self.city_game.computer_turn() is None:
+                print("Вы выиграли! Компьютер не смог назвать город.")
+                break
+
+            if self.city_game.check_game_over():
+                print("Компьютер выиграл. У вас больше нет возможных ходов.")
+                break
+            
