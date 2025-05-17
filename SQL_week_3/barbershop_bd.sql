f126accb80477514b039d027f01a060419a53f90
Ollie <Ollie_0@mail.ru>
2025-05-17 14:19:32 +0700

    removed extra lines

--- SQL_week_3/barbershop_bd.sql
+++ SQL_week_3/barbershop_bd.sql
@@ -17,11 +17,9 @@ CREATE TABLE IF NOT EXISTS appointments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     master_id INTEGER NOT NULL,
-    service_id INTEGER NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status TEXT NOT NULL CHECK (status IN ('pending', 'completed', 'canceled')),
-    FOREIGN KEY (master_id) REFERENCES masters(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (master_id) REFERENCES masters(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS masters_services (
