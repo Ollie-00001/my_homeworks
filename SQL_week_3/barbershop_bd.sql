a57c3472b1a7883299f9c2425f737741d3cceb7e
Ollie <Ollie_0@mail.ru>
2025-05-17 13:44:16 +0700

    Created table appointments

--- SQL_week_3/barbershop_bd.sql
+++ SQL_week_3/barbershop_bd.sql
@@ -12,3 +12,14 @@ CREATE TABLE IF NOT EXISTS services (
     description TEXT,
     price INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS appointments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    master_id INTEGER NOT NULL,
+    service_id INTEGER NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'completed', 'canceled')),
+    FOREIGN KEY (master_id) REFERENCES masters(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
