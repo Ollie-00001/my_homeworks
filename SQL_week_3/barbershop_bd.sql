5122bfac823d4a845bfaac50a445260d302a4847
Ollie <Ollie_0@mail.ru>
2025-05-17 13:37:36 +0700

    Created table services

--- SQL_week_3/barbershop_bd.sql
+++ SQL_week_3/barbershop_bd.sql
@@ -1,8 +1,14 @@
-CREATE TABLE IF NOT EXISTS masters
-(
+CREATE TABLE IF NOT EXISTS masters (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     middle_name TEXT,
     phone_number TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS services (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL UNIQUE,
+    description TEXT,
+    price INTEGER NOT NULL
 );
