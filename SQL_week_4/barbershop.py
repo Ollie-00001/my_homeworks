6274148242b4e1ef764f5cce266768d4f50e7e25
Ollie <Ollie_0@mail.ru>
2025-06-01 11:38:48 +0700

    fixed typos

--- SQL_week_4/barbershop.py
+++ SQL_week_4/barbershop.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import List
+from typing import List, Optional
 
 DB_PATH = 'barbershop_db.db'
 SQL_PATH = 'barbershop.sql'
@@ -14,20 +14,21 @@ def execute_script(conn: sqlite3.Connection, script: str) -> None:
 
 def find_appointment_by_phone(conn: sqlite3.Connection, phone: str) -> List[tuple]:
     query = '''
-    SELECT a.id, c.name, c.phone, m.name, a.comment, a.created_at
+    SELECT a.id, c.name, c.phone, b.name, a.comment, a.created_at
     FROM appointment a
     JOIN client c ON a.client_id = c.id
-    JOIN master m ON a.master_id = m.id
+    JOIN barber b ON a.barber_id = b.id
     WHERE c.phone = ?;
     '''
     return conn.execute(query, (phone,)).fetchall()
 
 def find_appointment_by_comment(conn: sqlite3.Connection, comment_part: str) -> List[tuple]:
     query = '''
-    SELECT a.id, c.name, c.phone, m.name, a.comment, a.created_at
+    SELECT a.id, c.name, c.phone, b.name, a.comment, a.created_at
     FROM appointment a
     JOIN client c ON a.client_id = c.id
-    JOIN master m ON a.master_id = m.id
+    JOIN barber b ON a.barber_id = b.id
     WHERE a.comment LIKE ?;
     '''
     return conn.execute(query, (f'%{comment_part}%',)).fetchall()
+
