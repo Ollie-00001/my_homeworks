51e454c5f5c52c7c5c2846aa4fba8d7161f293ed
Ollie <Ollie_0@mail.ru>
2025-06-01 10:58:50 +0700

    def execute_script

--- SQL_week_4/barbershop.py
+++ SQL_week_4/barbershop.py
@@ -8,3 +8,7 @@ def sql_file_read(filepath: str):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
     
+def execute_script(conn: sqlite3.Connection, script: str) -> None:
+    with conn:
+        conn.executescript(script)
+
