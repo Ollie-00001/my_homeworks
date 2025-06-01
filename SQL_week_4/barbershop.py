f1df550b162aa1c29fe320e9f3bd2d56abe581af
Ollie <Ollie_0@mail.ru>
2025-06-01 10:58:11 +0700

    def sql_file_read

--- SQL_week_4/barbershop.py
+++ SQL_week_4/barbershop.py
@@ -4,3 +4,7 @@ from typing import List
 DB_PATH = 'barbershop_db.db'
 SQL_PATH = 'barbershop.sql'
 
+def sql_file_read(filepath: str):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+    
