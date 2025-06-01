c572c9f963ad001ae8741a58e4415c9ae9eb5f34
Ollie <Ollie_0@mail.ru>
2025-06-01 11:51:27 +0700

    fixed typos in .py

--- SQL_week_4/barbershop.py
+++ SQL_week_4/barbershop.py
@@ -2,7 +2,7 @@ import sqlite3
 from typing import List, Optional
 
 DB_PATH = 'barbershop_db.db'
-SQL_PATH = 'barbershop.sql'
+SQL_PATH = r'C:\Main\my_projects\python_413\my_homeworks\SQL_week_4\barbershop.sql'
 
 def sql_file_read(filepath: str):
     with open(filepath, 'r', encoding='utf-8') as file:
@@ -15,9 +15,9 @@ def execute_script(conn: sqlite3.Connection, script: str) -> None:
 def find_appointment_by_phone(conn: sqlite3.Connection, phone: str) -> List[tuple]:
     query = '''
     SELECT a.id, c.name, c.phone, b.name, a.comment, a.created_at
-    FROM appointment a
-    JOIN client c ON a.client_id = c.id
-    JOIN barber b ON a.barber_id = b.id
+    FROM appointments a
+    JOIN clients c ON a.client_id = c.id
+    JOIN barbers b ON a.barber_id = b.id
     WHERE c.phone = ?;
     '''
     return conn.execute(query, (phone,)).fetchall()
@@ -25,9 +25,9 @@ def find_appointment_by_phone(conn: sqlite3.Connection, phone: str) -> List[tupl
 def find_appointment_by_comment(conn: sqlite3.Connection, comment_part: str) -> List[tuple]:
     query = '''
     SELECT a.id, c.name, c.phone, b.name, a.comment, a.created_at
-    FROM appointment a
-    JOIN client c ON a.client_id = c.id
-    JOIN barber b ON a.barber_id = b.id
+    FROM appointments a
+    JOIN clients c ON a.client_id = c.id
+    JOIN barbers b ON a.barber_id = b.id
     WHERE a.comment LIKE ?;
     '''
     return conn.execute(query, (f'%{comment_part}%',)).fetchall()
