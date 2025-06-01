4cf92acb60b620b97bcc29d6bb07136778800815
Ollie <Ollie_0@mail.ru>
2025-06-01 11:26:47 +0700

    two functions for fetching by phone and comment

--- SQL_week_4/barbershop.py
+++ SQL_week_4/barbershop.py
@@ -12,3 +12,22 @@ def execute_script(conn: sqlite3.Connection, script: str) -> None:
     with conn:
         conn.executescript(script)
 
+def find_appointment_by_phone(conn: sqlite3.Connection, phone: str) -> List[tuple]:
+    query = '''
+    SELECT a.id, c.name, c.phone, m.name, a.comment, a.created_at
+    FROM appointment a
+    JOIN client c ON a.client_id = c.id
+    JOIN master m ON a.master_id = m.id
+    WHERE c.phone = ?;
+    '''
+    return conn.execute(query, (phone,)).fetchall()
+
+def find_appointment_by_comment(conn: sqlite3.Connection, comment_part: str) -> List[tuple]:
+    query = '''
+    SELECT a.id, c.name, c.phone, m.name, a.comment, a.created_at
+    FROM appointment a
+    JOIN client c ON a.client_id = c.id
+    JOIN master m ON a.master_id = m.id
+    WHERE a.comment LIKE ?;
+    '''
+    return conn.execute(query, (f'%{comment_part}%',)).fetchall()
