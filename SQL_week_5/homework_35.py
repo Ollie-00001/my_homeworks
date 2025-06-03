def3f0a0fdd3d6600997a6942ebd4140e2e40c13
Ollie <Ollie_0@mail.ru>
2025-06-03 15:45:37 +0700

    appointments table

--- SQL_week_5/homework_35.py
+++ SQL_week_5/homework_35.py
@@ -17,3 +17,10 @@ class Service(BaseModel):
     title: str = CharField(max_length=100, unique=True)
     description: str = TextField(null=True)
     price: DecimalField = DecimalField(max_digits=7, decimal_places=2)
+
+class Appointment(BaseModel):
+    client_name: str = CharField(max_length=100, null=False)
+    client_phone: str = CharField(max_length=20, null=False)
+    date: datetime = DateTimeField(default=datetime.now)
+    master = ForeignKeyField(Master, backref='appointments')
+    status: str = CharField(max_length=20, default='pending')
