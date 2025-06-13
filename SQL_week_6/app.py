ee650dcc7a37233fc4bab1c26c3664432ba7d77b
Ollie <Ollie_0@mail.ru>
2025-06-13 15:25:06 +0700

    DB, Base and  Master model

--- SQL_week_6/app.py
+++ SQL_week_6/app.py
@@ -4,6 +4,17 @@ from peewee import *
 # DB and Models
 DB = SqliteDatabase('barbershop_peewee.db')
 
+class BaseModel(Model):
+    class Meta:
+        database = DB
+
+class Master(BaseModel):
+    first_name = CharField(max_length = 50, null=False)
+    last_name = CharField(max_length=50, null=False)
+    middle_name = CharField(max_length=50, null=True)
+    phone = CharField(max_length=16, unique=True)
+
+
 
 # Flask Application
 app = Flask(__name__)
