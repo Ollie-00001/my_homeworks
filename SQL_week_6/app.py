5b52647b761a316f6a1b4775d8425a2c945c3f28
Ollie <Ollie_0@mail.ru>
2025-06-13 15:35:13 +0700

    Service and Appointment models

--- SQL_week_6/app.py
+++ SQL_week_6/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from peewee import *
+from datetime import datetime
 
 # DB and Models
 DB = SqliteDatabase('barbershop_peewee.db')
@@ -9,12 +10,22 @@ class BaseModel(Model):
         database = DB
 
 class Master(BaseModel):
-    first_name = CharField(max_length = 50, null=False)
-    last_name = CharField(max_length=50, null=False)
+    first_name = CharField(max_length = 50)
+    last_name = CharField(max_length=50)
     middle_name = CharField(max_length=50, null=True)
     phone = CharField(max_length=16, unique=True)
 
+class Service(BaseModel):
+    title = CharField(max_length=50, unique=True)
+    description = TextField(max_length=100, null=True)
+    price = DecimalField(max_digits=5, decimal_places=2)
 
+class Appointment(BaseModel):
+    client_name = CharField(max_length=50)
+    client_phone = CharField(max_length=16)
+    datetime = DateTimeField(default=datetime.now)
+    master = ForeignKeyField(Master, backref='appointments')
+    status = CharField(max_length=20, default='pending')
 
 # Flask Application
 app = Flask(__name__)
