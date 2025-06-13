81f9de1380251b61a41dc336fe8f55ab6e29a9b7
Ollie <Ollie_0@mail.ru>
2025-06-13 15:19:01 +0700

    Flask App initialization

--- SQL_week_6/app.py
+++ SQL_week_6/app.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from peewee import *
 
+# DB and Models
+DB = SqliteDatabase('barbershop_peewee.db')
+
+
+# Flask Application
 app = Flask(__name__)
+
+@app.before_request
+def db_connect():
+    DB.connect()
+
+@app.teardown_request
+def db_close():
+    DB.close()
 
 @app.route("/")
 def main():
