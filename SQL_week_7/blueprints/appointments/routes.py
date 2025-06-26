164e5f35005112214ee88ea40da78d5747f03d4a
Ollie <Ollie_0@mail.ru>
2025-06-26 09:42:10 +0700

    POST and GET for appointments and masters

--- SQL_week_7/blueprints/appointments/routes.py
+++ SQL_week_7/blueprints/appointments/routes.py
@@ -10,13 +10,46 @@
 """
 
 from flask import Blueprint, request, jsonify
-from models import Master
+from models import Master, Service, Appointment, MasterService, AppointmentService
+from auth import is_valid_api_key, is_admin
+from peewee import DoesNotExist, IntegrityError
 
-# Создание блюпринта с префиксом /masters
 appointments_bp = Blueprint('appointments', __name__, url_prefix='/appointments')
 
-# Перенесите сюда соответствующие маршруты
 @appointments_bp.route('/', methods=['GET'])
 def get_appointments():
-    # логика получения списка мастеров
+
+    api_key = request.headers.get('X-API-KEY')
+
+    if not is_valid_api_key(api_key):
+        return jsonify({'error': 'Invalid API key'}), 401
+    
+    appointments = Appointment.select().join(AppointmentService).join(Service).join(Master)
+
+    appointments_list = [
+        {
+            'id': appointment.id,
+            'master_id': appointment.master_id,
+            'client_name': appointment.client_name,
+            'client_phone': appointment.client_phone,
+            'comment': appointment.comment,
+            'status': appointment.status,
+            'master': appointment.master.name,
+            'services': [service.title for service in appointment.services]
+        }
+        for appointment in appointments
+    ]
+
+    return jsonify(appointments_list), 200
+
+@appointments_bp.route('/appointments/<id>', methods=['GET'])
+def get_appointment_by_id(id):
+    pass
+
+@appointments_bp.route('/appointments/master/<master_id>', methods=['GET'])
+def get_appointments_by_master(master_id):
+    pass
+
+@appointments_bp.route('/appointments', methods=['POST'])
+def create_appointment():
     pass
--- SQL_week_7/blueprints/masters/routes.py
+++ SQL_week_7/blueprints/masters/routes.py
@@ -9,13 +9,42 @@
 """
 
 from flask import Blueprint, request, jsonify
-from models import Master
+from models import Master, Service, Appointment, MasterService, AppointmentService
+from auth import is_valid_api_key, is_admin
+from peewee import DoesNotExist, IntegrityError
 
-# Создание блюпринта с префиксом /masters
 masters_bp = Blueprint('masters', __name__, url_prefix='/masters')
 
-# Перенесите сюда соответствующие маршруты
 @masters_bp.route('/', methods=['GET'])
 def get_masters():
-    # логика получения списка мастеров
+    """
+    Контроллер, обрабатывающий GET-запрос по маршруту /masters
+    """
+    api_key = request.headers.get('X-API-KEY')
+
+    # Проверка API-ключа
+    if not is_valid_api_key(api_key):
+        return jsonify({'error': 'Invalid API key'}), 401
+    
+    masters = Master.select()
+
+    masters_list = [
+        {
+            'id': master.id,
+            'first_name': master.name,
+            'last_name': master.last_name,
+            'middle_name': master.middle_name,
+            'phone': master.phone,
+        } 
+        for master in masters
+    ]
+
+    return jsonify(masters_list), 200
+
+@masters_bp.route('/masters/<id>', methods=['GET'])
+def get_master_by_id(id):
+    pass
+
+@masters_bp.route('/masters', methods=['POST'])
+def create_master():
     pass
